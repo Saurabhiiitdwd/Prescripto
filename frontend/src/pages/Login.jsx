28e18a1411fac132efe849e5795c2bf70857a483
Saurabh Singh <saurabh9120390366@gmail.com>
2026-06-16 16:13:02 +0530

    Implement Login form with state management and conditional rendering for Sign Up and Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Login() {
+  const [state, setState]= useState('Sign Up');
+
+  const [email, setEmail]= useState('');
+  const [password, setPassword]= useState('');
+  const [name, setName]= useState('');
+
+  const onSubmitHandler= async(event)=>{
+    event.preventDefault();     // prevent default form submission behavior
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmitHandler} className= 'min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</p>
+        <p>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book an appointment</p>
+        {
+          state === 'Sign Up' && 
+          <div className='w-full'>
+            <p>Full Name</p>
+            <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='text' onChange={(e)=> setName(e.target.value)} value={name} />
+          </div>
+        }
+        <div className='w-full'>
+          <p>Email</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='email' onChange={(e)=> setEmail(e.target.value)} value={email} />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1'  type='password' onChange={(e)=> setPassword(e.target.value)} value={password} />
+        </div>
+        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</button>
+        {
+          state === 'Sign Up' ? (
+            <p className='text-sm text-center'>
+              Already have an account?{' '}
+              <button type="button" className='text-primary hover:underline' onClick={() => setState('Login')}>
+                Login
+              </button>
+            </p>
+          ) : (
+            <p className='text-sm text-center'>
+              Don't have an account?{' '}
+              <button type="button" className='text-primary hover:underline' onClick={() => setState('Sign Up')}>
+                Sign Up
+              </button>
+            </p>
+          )
+        }
+      </div>
+    </form>
   )
 }
 
