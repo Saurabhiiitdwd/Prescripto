74ee16a9b473676b1ec085a9d831903f55c8f5a6
Saurabh Singh <saurabh9120390366@gmail.com>
2026-06-23 15:33:53 +0530

    Add responsive mobile menu to Navbar and implement filter toggle in Doctors page

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 function Doctors() {
   const {speciality} = useParams();
   const [filteredDoctors, setFilteredDoctors] = useState([]);
+  const [showFilter, setShowFilter]= useState(false);
   const {doctors} = useContext(AppContext);
   const navigate = useNavigate();
 
@@ -26,7 +27,8 @@ function Doctors() {
     <div>
       <p className='text-grey-600'>Browse through the doctors specialist.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <button onClick={() => setShowFilter(prev => !prev)}className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`}>Filters</button>
+        <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
           <p onClick={() => speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
           <p onClick={() => speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
           <p onClick={() => speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>Dermatologist</p>
@@ -36,7 +38,7 @@ function Doctors() {
         </div>
         <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>{
           filteredDoctors.map((item, index)=>(
-          <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all-duration-500' key={index} onClick={() => navigate(`/appointment/${item._id}`)}>
+          <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} onClick={() => navigate(`/appointment/${item._id}`)}>
             <img className='bg-blue-50' src={item.image} alt={item.name} />
             <div className='p-4'>
               <div className='flex items-center gap-2 text-green-500 text-sm text-center'>
