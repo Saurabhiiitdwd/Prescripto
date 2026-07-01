75b9e8910607182e793651b11155b7f9bcc41e1e
Saurabh Singh <saurabh9120390366@gmail.com>
2026-07-01 21:47:24 +0530

    feat: add initial setup for admin panel with React, Vite, and TailwindCSS
    - Added React and Vite configuration for the admin panel.
    - Integrated TailwindCSS for styling.
    - Created SVG assets for React and Vite logos.
    - Implemented main entry point for the React application.
    - Developed adminController for handling doctor addition and admin login.
    - Added authentication middleware for admin routes.
    - Set up multer for image uploads.
    - Created user model for MongoDB interactions.
    - Established routes for admin functionalities including adding doctors and admin login.

---
+++ admin/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div class="text-3xl font-bold underline">App</div>
+  )
+}
+
+export default App
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 const app=express();
 const PORT= process.env.PORT || 4000
@@ -14,6 +15,10 @@ app.use(express.json());
 app.use(cors())
 
 //api endpoints
+
+app.use('/api/admin', adminRouter)
+//localhost:4000/api/admin/add-doctor
+
 app.get('/', (req, res)=>{
   res.send('API Working')
 })
